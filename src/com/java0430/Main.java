cb966c8c318bafcc56f1dd7bd1023d4991778c10
GD3 <GD3@DESKTOP-1R9S08D>
2019-04-30 16:28:42 +0900

    Java EE 시작

---
+++ src/com/java0430/Main.java
@@ -0,0 +1,21 @@
+package com.java0430;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main") // URL : http://localhost:8080/201905/Main
+public class Main extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
